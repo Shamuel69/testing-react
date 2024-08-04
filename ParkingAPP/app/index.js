6ea9ea10e9abc73c72b22240b4353c0f9293000e
Samuel the Ultimate <tobbanganmantis@gmail.com>
2024-08-04 03:16:36 -0400

    bingus

--- ParkingAPP/app/index.js
+++ ParkingAPP/app/index.js
@@ -1,26 +1,40 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar } from 'expo-status-bar'; 
 import { Link } from 'expo-router';
 export default function Home(){
     return (
+      <View>
+        <LinearGradient colors={['#18151C', '#181028', "", '#281649']} style={{height: "100%"}}>
+        <View style={styles.header}>
+            <Text>Parksie</Text>
+        </View>
         <View style={styles.container}>
-          <Text style={styles.title} >TEST!
+          <Text style={styles.title}>TEST!
             <View style={styles.inline_container}>
-
             </View>
           </Text>
-        <StatusBar style="auto"/>
-        
         </View>
-    )
+        </LinearGradient>
+      </View>
+        
+      
+  )
 }
 
 
 const styles = StyleSheet.create({
+    header: {
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      borderBottomColor: "white",
+    },
     container: {
-      backgroundColor: 'rgb(42, 34, 54)',
-
+      backgroundColor: '#251F30',
+      
       flex:1,
       alignItems: "center",
       justifyContent: "center",
